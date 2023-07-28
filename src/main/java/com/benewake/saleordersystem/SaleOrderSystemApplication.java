93f20337582734539b4964e372fb0692b43c78cc
ste519 <qianzi@umich.edu>
2023-07-28 09:14:55 +0800

    Local setup

--- src/main/java/com/benewake/saleordersystem/SaleOrderSystemApplication.java
+++ src/main/java/com/benewake/saleordersystem/SaleOrderSystemApplication.java
@@ -3,8 +3,16 @@ package com.benewake.saleordersystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 /**
  * @author Lcs
  */
@@ -17,4 +25,27 @@ public class SaleOrderSystemApplication {
         SpringApplication.run(SaleOrderSystemApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));  // Set your frontend's url here
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type")); // You don't need to add access-control-allow-origin here
+        configuration.setExposedHeaders(Arrays.asList("Content-Type", "Set-Cookie"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+            new CorsFilter(source));
+
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
+    
+
+
+
+
